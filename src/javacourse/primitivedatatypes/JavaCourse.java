57205ed076159817403503436b8956c7973dc3d2
Rajaram Rawool <rajaram903@gamil.com>
2021-10-16 00:36:15 +0530

    All 8 Primitive Data types

--- src/javacourse/primitivedatatypes/JavaCourse.java
+++ src/javacourse/primitivedatatypes/JavaCourse.java
@@ -4,9 +4,9 @@ public class JavaCourse {
     public static void main(String[] args) {
         System.out.println("Hi, Rajaram");
         int age = 28;
-        int firstNumber = 35 , secondNumber = 12, thirdNumber = 6;
+        int firstNumber = 35, secondNumber = 12, thirdNumber = 6;
         int total = firstNumber + secondNumber + thirdNumber;
-        int lastNumber = 1000 - total ;
+        int lastNumber = 1000 - total;
         System.out.println("My Age is : " + age);
         System.out.println(total + " and " + lastNumber);
 
@@ -33,16 +33,30 @@ public class JavaCourse {
         System.out.println("---Integer Primitive Data type---");
         System.out.println("Minimum Value for Integer = " + minValueInteger);
         System.out.println("Maximum Value for Integer = " + maxValueInteger);
-        System.out.println();
+
         /*Long*/
+        System.out.println();
         long minValueLong = Long.MIN_VALUE;
         long maxValueLong = Long.MAX_VALUE;
         System.out.println("---Long Primitive Data type---");
         System.out.println("Minimum Value for Long = " + minValueLong);
         System.out.println("Maximum Value for Long = " + maxValueLong);
 
+        /*Float*/
+        System.out.println();
+        float minValueFloat = Float.MIN_VALUE;
+        float maxValueFloat = Float.MAX_VALUE;
+        System.out.println("---Float Primitive Data type---");
+        System.out.println("Minimum Value for Float = " + minValueFloat);
+        System.out.println("Maximum Value for Float = " + maxValueFloat);
 
-
+        /*Double*/
+        System.out.println();
+        double minValueDouble = Double.MIN_VALUE;
+        double maxValueDouble = Double.MAX_VALUE;
+        System.out.println("---Double Primitive Data type---");
+        System.out.println("Minimum Value for Double = " + minValueDouble);
+        System.out.println("Maximum Value for Double = " + maxValueDouble);
 
 
     }
